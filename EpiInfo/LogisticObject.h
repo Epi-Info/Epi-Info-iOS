a5168cc30c3dcc6eef1943ecf2c4efb068348830
zfj4 <zfj4@cdc.gov>
2018-10-16 08:26:53 -0400

    Added LogisticObject class

---
+++ EpiInfo/LogisticObject.h
@@ -0,0 +1,16 @@
+//
+//  LogisticObject.h
+//  EpiInfo
+//
+//  Created by John Copeland on 10/16/18.
+//
+
+#import "TablesObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LogisticObject : TablesObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ EpiInfo/LogisticObject.m
@@ -0,0 +1,12 @@
+//
+//  LogisticObject.m
+//  EpiInfo
+//
+//  Created by John Copeland on 10/16/18.
+//
+
+#import "LogisticObject.h"
+
+@implementation LogisticObject
+
+@end
