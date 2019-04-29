24a6f5600f09703d9c97f5434ef4625df42532ee
zfj4 <zfj4@cdc.gov>
2019-04-29 16:22:33 -0400

    Form Designer: Continued Date Field Check Code

--- EpiInfo/FormDesigner/CheckCodeWriter.m
+++ EpiInfo/FormDesigner/CheckCodeWriter.m
@@ -27,6 +27,7 @@
         [self.beforeButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateDisabled];
         [self.beforeButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:16.0]];
         [self.beforeButton addTarget:self action:@selector(beforeOrAfterButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+        [self.beforeButton setEnabled:NO];
         [self addSubview:self.beforeButton];
         
         self.afterButton = [[UIButton alloc] initWithFrame:CGRectMake(frame.size.width / 2.0, 0, frame.size.width / 2.0, 32)];
@@ -71,22 +72,64 @@
 
 - (void)beforeOrAfterButtonPressed:(UIButton *)sender
 {
+    UIView *selectFunctionView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, 0)];
+    [selectFunctionView setBackgroundColor:[UIColor whiteColor]];
+    [self addSubview:selectFunctionView];
     if ([[[sender titleLabel] text] isEqualToString:@"After"])
     {
-        NSLog(@"After button pressed");
+        if (YES)
+        {
+            UIButton *differenceInYearsButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, selectFunctionView.frame.size.width / 2.0, 32)];
+            [differenceInYearsButton setBackgroundColor:[UIColor whiteColor]];
+            [differenceInYearsButton setTitle:@"Years" forState:UIControlStateNormal];
+            [differenceInYearsButton setTitleColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0] forState:UIControlStateNormal];
+            [differenceInYearsButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateHighlighted];
+            [differenceInYearsButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateDisabled];
+            [differenceInYearsButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:16.0]];
+            [differenceInYearsButton addTarget:self action:@selector(functionSelectionButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+            [selectFunctionView addSubview:differenceInYearsButton];
+            [selectFunctionView setTag:0];
+        }
     }
     else
     {
         NSLog(@"Before button pressed");
     }
+    UIButton *closeButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, selectFunctionView.frame.size.width / 2.0, 32)];
+    [closeButton setBackgroundColor:[UIColor whiteColor]];
+    [closeButton setTitle:@"Close" forState:UIControlStateNormal];
+    [closeButton setTitleColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0] forState:UIControlStateNormal];
+    [closeButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateHighlighted];
+    [closeButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateDisabled];
+    [closeButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:16.0]];
+    [closeButton addTarget:self action:@selector(closeButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [selectFunctionView addSubview:closeButton];
+    [UIView animateWithDuration:0.2 delay:0.0 options:UIViewAnimationOptionCurveLinear animations:^{
+        [selectFunctionView setFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+        [closeButton setFrame:CGRectMake(closeButton.frame.origin.x, selectFunctionView.frame.size.height - 32, closeButton.frame.size.width, 32)];
+    } completion:^(BOOL finished){
+    }];
+}
+
+- (void)functionSelectionButtonPressed:(UIButton *)sender
+{
+    NSLog(@"%@", [[sender titleLabel] text]);
 }
 
 - (void)closeButtonPressed:(UIButton *)sender
 {
     [UIView animateWithDuration:0.2 delay:0.0 options:UIViewAnimationOptionCurveLinear animations:^{
-        [self setFrame:CGRectMake(1, 1, self.frame.size.width, 0)];
+        [[sender superview] setFrame:CGRectMake([sender superview].frame.origin.x, [sender superview].frame.origin.y, [sender superview].frame.size.width, 0)];
+        if ([sender superview] != self)
+        {
+            UIView *sv = [sender superview];
+            for (UIView *v in [sv subviews])
+            {
+                [v setFrame:CGRectMake(v.frame.origin.x, 0, v.frame.size.width, 32)];
+            }
+        }
     } completion:^(BOOL finished){
-        [self removeFromSuperview];
+        [[sender superview] removeFromSuperview];
     }];
 }
 
