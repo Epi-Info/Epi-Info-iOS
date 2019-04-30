10611f59fc2e02d6651efefc4c32729513eb5423
zfj4 <zfj4@cdc.gov>
2019-04-30 14:58:41 -0400

    Continued Form Designer Check Code

--- EpiInfo/FormDesigner/CheckCodeWriter.m
+++ EpiInfo/FormDesigner/CheckCodeWriter.m
@@ -65,6 +65,8 @@
         [beforeButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateHighlighted];
         [beforeButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateDisabled];
         [beforeButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:18.0]];
+        [beforeButton.layer setValue:fn forKey:@"FieldName"];
+        [beforeButton.layer setValue:ft forKey:@"FieldType"];
         [beforeButton addTarget:self action:@selector(beforeOrAfterButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
         [beforeButton setEnabled:NO];
         [self addSubview:beforeButton];
@@ -76,6 +78,8 @@
         [afterButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateHighlighted];
         [afterButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateDisabled];
         [afterButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:18.0]];
+        [afterButton.layer setValue:fn forKey:@"FieldName"];
+        [afterButton.layer setValue:ft forKey:@"FieldType"];
         [afterButton addTarget:self action:@selector(beforeOrAfterButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:afterButton];
         
@@ -97,33 +101,95 @@
     UIView *selectFunctionView = [[UIView alloc] initWithFrame:CGRectMake(0, -self.frame.size.height, self.frame.size.width, self.frame.size.height)];
     [selectFunctionView setBackgroundColor:[UIColor whiteColor]];
     [self addSubview:selectFunctionView];
+    
+    UILabel *firstLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, 32)];
+    [firstLabel setFont:[UIFont fontWithName:@"HelveticaNeue-Bold" size:18.0]];
+    [firstLabel setTextColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0]];
+    [firstLabel setTextAlignment:NSTextAlignmentCenter];
+    [firstLabel setText:@"Check Code Builder"];
+    [selectFunctionView addSubview:firstLabel];
+    
+    UILabel *secondLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 32, self.frame.size.width / 2.0 - 4, 32)];
+    [secondLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:18.0]];
+    [secondLabel setTextColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0]];
+    [secondLabel setTextAlignment:NSTextAlignmentRight];
+    [secondLabel setText:@"Field Name:"];
+    [selectFunctionView addSubview:secondLabel];
+    
+    UILabel *thirdLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.frame.size.width / 2.0 + 4, 32, self.frame.size.width / 2.0 - 4, 32)];
+    [thirdLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:16.0]];
+    [thirdLabel setTextColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0]];
+    [thirdLabel setTextAlignment:NSTextAlignmentLeft];
+    [thirdLabel setText:[sender.layer valueForKey:@"FieldName"]];
+    [selectFunctionView addSubview:thirdLabel];
+    
+    UILabel *fourthLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 64, self.frame.size.width / 2.0 - 4, 32)];
+    [fourthLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:18.0]];
+    [fourthLabel setTextColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0]];
+    [fourthLabel setTextAlignment:NSTextAlignmentRight];
+    [fourthLabel setText:@"Field Type:"];
+    [selectFunctionView addSubview:fourthLabel];
+    
+    UILabel *fifthLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.frame.size.width / 2.0 + 4, 64, self.frame.size.width / 2.0 - 4, 32)];
+    [fifthLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:16.0]];
+    [fifthLabel setTextColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0]];
+    [fifthLabel setTextAlignment:NSTextAlignmentLeft];
+    [fifthLabel setText:[sender.layer valueForKey:@"FieldType"]];
+    [selectFunctionView addSubview:fifthLabel];
+    
+    UILabel *sixthLabel = [[UILabel alloc] initWithFrame:CGRectMake(8, 96, self.frame.size.width - 16, 32)];
+    [sixthLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:18.0]];
+    [sixthLabel setTextColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0]];
+    [sixthLabel setTextAlignment:NSTextAlignmentLeft];
+    [sixthLabel setText:@"Select a function to execute:"];
+    [selectFunctionView addSubview:sixthLabel];
+
     if ([[[sender titleLabel] text] isEqualToString:@"After"])
     {
-        if (YES)
+        if ([[sender.layer valueForKey:@"FieldType"] isEqualToString:@"Date"])
         {
-            UIButton *differenceInYearsButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, selectFunctionView.frame.size.width / 2.0, 32)];
+            UIButton *differenceInYearsButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 128, selectFunctionView.frame.size.width / 2.0, 32)];
             [differenceInYearsButton setBackgroundColor:[UIColor whiteColor]];
             [differenceInYearsButton setTitle:@"Years" forState:UIControlStateNormal];
             [differenceInYearsButton setTitleColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0] forState:UIControlStateNormal];
             [differenceInYearsButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateHighlighted];
             [differenceInYearsButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateDisabled];
-            [differenceInYearsButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:16.0]];
+            [differenceInYearsButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:18.0]];
             [differenceInYearsButton addTarget:self action:@selector(functionSelectionButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
             [selectFunctionView addSubview:differenceInYearsButton];
-            [selectFunctionView setTag:0];
+            
+            UIButton *differenceInMonthsButton = [[UIButton alloc] initWithFrame:CGRectMake(selectFunctionView.frame.size.width / 2.0, 128, selectFunctionView.frame.size.width / 2.0, 32)];
+            [differenceInMonthsButton setBackgroundColor:[UIColor whiteColor]];
+            [differenceInMonthsButton setTitle:@"Months" forState:UIControlStateNormal];
+            [differenceInMonthsButton setTitleColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0] forState:UIControlStateNormal];
+            [differenceInMonthsButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateHighlighted];
+            [differenceInMonthsButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateDisabled];
+            [differenceInMonthsButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:18.0]];
+            [differenceInMonthsButton addTarget:self action:@selector(functionSelectionButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+            [selectFunctionView addSubview:differenceInMonthsButton];
+            
+            UIButton *differenceInDaysButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 160, selectFunctionView.frame.size.width / 2.0, 32)];
+            [differenceInDaysButton setBackgroundColor:[UIColor whiteColor]];
+            [differenceInDaysButton setTitle:@"Days" forState:UIControlStateNormal];
+            [differenceInDaysButton setTitleColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0] forState:UIControlStateNormal];
+            [differenceInDaysButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateHighlighted];
+            [differenceInDaysButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateDisabled];
+            [differenceInDaysButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:18.0]];
+            [differenceInDaysButton addTarget:self action:@selector(functionSelectionButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+            [selectFunctionView addSubview:differenceInDaysButton];
         }
     }
     else
     {
         NSLog(@"Before button pressed");
     }
-    UIButton *closeButton = [[UIButton alloc] initWithFrame:CGRectMake(0, selectFunctionView.frame.size.height - 32, selectFunctionView.frame.size.width / 2.0, 32)];
+    UIButton *closeButton = [[UIButton alloc] initWithFrame:CGRectMake(selectFunctionView.frame.size.width / 2.0, selectFunctionView.frame.size.height - 48, selectFunctionView.frame.size.width / 2.0, 32)];
     [closeButton setBackgroundColor:[UIColor whiteColor]];
     [closeButton setTitle:@"Close" forState:UIControlStateNormal];
     [closeButton setTitleColor:[UIColor colorWithRed:88/255.0 green:89/255.0 blue:91/255.0 alpha:1.0] forState:UIControlStateNormal];
     [closeButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateHighlighted];
     [closeButton setTitleColor:[UIColor colorWithRed:188/255.0 green:190/255.0 blue:192/255.0 alpha:1.0] forState:UIControlStateDisabled];
-    [closeButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:16.0]];
+    [closeButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:18.0]];
     [closeButton addTarget:self action:@selector(closeButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
     [selectFunctionView addSubview:closeButton];
     [UIView animateWithDuration:0.2 delay:0.0 options:UIViewAnimationOptionCurveLinear animations:^{
